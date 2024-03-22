2f429813d347920d81ea39801b30534d3453359e
Meriç ALKIŞLA <70147993+MAlkisla@users.noreply.github.com>
2024-03-22 22:24:16 +0300

    Adapter updated

--- app/src/main/java/com/example/foodorderapplication/ui/adapter/FoodsAdapter.kt
+++ app/src/main/java/com/example/foodorderapplication/ui/adapter/FoodsAdapter.kt
@@ -1,21 +1,28 @@
 package com.example.foodorderapplication.ui.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodorderapplication.data.entity.Foods
 import com.example.foodorderapplication.databinding.HomepageCardDesignBinding
+import com.example.foodorderapplication.ui.fragment.HomepageFragmentDirections
+import com.google.android.material.snackbar.Snackbar
 
-class FoodsAdapter(var mContext: Context, var foodsList: List<Foods>): RecyclerView
-    .Adapter<FoodsAdapter.HomepageCardDesingHolder>() {
+class FoodsAdapter(var mContext: Context, var foodsList: List<Foods>) : RecyclerView
+.Adapter<FoodsAdapter.HomepageCardDesingHolder>() {
     inner class HomepageCardDesingHolder(var design: HomepageCardDesignBinding) : RecyclerView
     .ViewHolder(design.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomepageCardDesingHolder {
-        val binding = HomepageCardDesignBinding.inflate(LayoutInflater.from(mContext),parent,false)
+        val binding =
+            HomepageCardDesignBinding.inflate(LayoutInflater.from(mContext), parent, false)
         return HomepageCardDesingHolder(binding)
     }
+
     override fun onBindViewHolder(holder: HomepageCardDesingHolder, position: Int) {
         val food = foodsList.get(position)
         val design = holder.design
@@ -29,9 +36,50 @@ class FoodsAdapter(var mContext: Context, var foodsList: List<Foods>): RecyclerV
         )
 
         design.textViewFoodPrice.text = food.food_price.toString()
+
+        design.cardView.setOnClickListener {
+            val amountString = design.textViewAmountText.text.toString()
+            val amount = amountString.toIntOrNull() ?: 1
+
+            val action = HomepageFragmentDirections.actionDetail(food = food, amount = amount)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        design.cardButtonDeactive.setOnClickListener {
+            design.cardButtonDeactive.visibility = View.INVISIBLE
+            design.cardButtonActive.visibility = View.VISIBLE
+            //TODO: Sepete eklencek ve sepette ise active kalacak
+        }
+        design.imageViewIncreaseButton.setOnClickListener {
+            val amountString = design.textViewAmountText.text.toString()
+            val currentAmount = amountString.toIntOrNull() ?: 1
+            val newAmount = currentAmount + 1
+            design.textViewAmountText.text = newAmount.toString()
+        }
+
+        design.imageViewReduceButton.setOnClickListener {
+            val amountString = design.textViewAmountText.text.toString()
+            val currentAmount = amountString.toIntOrNull() ?: 1
+            val newAmount = currentAmount - 1
+            design.textViewAmountText.text = newAmount.toString()
+            if (newAmount <= 0) {
+                Snackbar.make(
+                    it, "${food.food_name} sepetten kaldırıldı", Snackbar
+                        .LENGTH_SHORT
+                ).show()
+                remove(food.food_id)
+                design.textViewAmountText.text = "1"
+                design.cardButtonDeactive.visibility = View.VISIBLE
+                design.cardButtonActive.visibility = View.INVISIBLE
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return foodsList.size
+    }
+
+    fun remove(food_id: Int) {
+        Log.e("Yemek Kaldır", food_id.toString())
     }
 }
